7987a25b95f66c8cb96151e760195a8201d076c5
SkrowRepap <Iamblank015@gmail.com>
2020-12-14 21:15:09 +0800

    working

--- Bookaholics/queries/connection.php
+++ Bookaholics/queries/connection.php
@@ -6,6 +6,12 @@
 	$dbpass = '';
 	$dbname = 'books';
 
+
+	// $dbhost = 'sql110.epizy.com';
+	// $dbuser = 'epiz_27047207';
+	// $dbpass = 'uhZu4O9CyUf2j';
+	// $dbname = 'epiz_27047207_books';
+	
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
     // Check connection
